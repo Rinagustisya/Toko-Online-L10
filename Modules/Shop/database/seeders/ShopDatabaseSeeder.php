f137b174c3bb8d5037820e2533083c709bd78866
MyName269 <rinaagustisya885@gmail.com>
2023-11-13 09:45:20 +0700

    feat : Modularisasi dengan Package laravel-modules

---
+++ Modules/Shop/database/seeders/ShopDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Shop\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class ShopDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Shop/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('shop::layouts.master')
+
+@section('content')
+    <h1>Hello World! Shop</h1>
+
+    <p>Module: {!! config('shop.name') !!}</p>
+@endsection
